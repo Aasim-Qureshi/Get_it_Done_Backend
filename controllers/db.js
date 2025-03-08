66366e49a05fe8254f31f3a487b0e7d4201512a6
Aasim-Qureshi <aasimq194@gmail.com>
2025-03-08 12:26:25 +0530

    Deploy changes

--- controllers/db.js
+++ controllers/db.js
@@ -3,12 +3,6 @@ const { Pool } = require('pg');
 const pool = new Pool({
 
     connectionString: process.env.DATABASE_URL,
-    user: process.env.USERNAME,
-    host: process.env.HOSTNAME,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DB_PORT,
-
     ssl: {
         rejectUnauthorized: false
     }
